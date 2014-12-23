e5dd42b392b6aaef55e56439c4446fc97bb1e1b6
Neal Harris <neal.harris@gmail.com>
2014-12-23 09:55:16 -0800

    put basic utilities in their own file

--- matasano.go
+++ matasano.go
@@ -7,7 +7,6 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"errors"
-	"fmt"
 	"math"
 	mathrand "math/rand"
 	"reflect"
@@ -15,43 +14,6 @@ import (
 	"strings"
 	"time"
 )
-
-func HexToB64(s string) (string, error) {
-	bytes, err := hex.DecodeString(s)
-	if err != nil {
-		fmt.Println("error: ", err)
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(bytes), nil
-}
-
-func B64ToHex(s string) (string, error) {
-	bytes, err := base64.StdEncoding.DecodeString(s)
-	if err != nil {
-		fmt.Println("error: ", err)
-		return "", err
-	}
-	return hex.EncodeToString(bytes), nil
-}
-
-func Xor(b1 []byte, b2 []byte) ([]byte, error) {
-	if len(b1) != len(b2) {
-		return nil, errors.New("byte arrays not the same length")
-	}
-
-	result := make([]byte, len(b1))
-
-	for i := 0; i < len(b1); i++ {
-		result[i] = b1[i] ^ b2[i]
-	}
-
-	return result, nil
-}
-
-func SingleCharXor(b byte, s []byte) ([]byte, error) {
-	repeated := bytes.Repeat([]byte{b}, len(s))
-	return Xor(repeated, s)
-}
 
 func RuneFrequencies(b []byte) map[rune]float64 {
 	counts := make(map[rune]int)
---
+++ utilities.go
@@ -0,0 +1,46 @@
+package matasano
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
+	"fmt"
+)
+
+func HexToB64(s string) (string, error) {
+	bytes, err := hex.DecodeString(s)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(bytes), nil
+}
+
+func B64ToHex(s string) (string, error) {
+	bytes, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		fmt.Println("error: ", err)
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
+
+func Xor(b1 []byte, b2 []byte) ([]byte, error) {
+	if len(b1) != len(b2) {
+		return nil, errors.New("byte arrays not the same length")
+	}
+
+	result := make([]byte, len(b1))
+
+	for i := 0; i < len(b1); i++ {
+		result[i] = b1[i] ^ b2[i]
+	}
+
+	return result, nil
+}
+
+func SingleCharXor(b byte, s []byte) ([]byte, error) {
+	repeated := bytes.Repeat([]byte{b}, len(s))
+	return Xor(repeated, s)
+}
